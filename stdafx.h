baaf57c7892f0ccef1c5cf6493b6bad0c003ea24
jamesj <ehddnr177@naver.com>
2019-07-25 13:45:18 +0900

    ADD stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,19 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include <pcap.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <libnet.h>
+#include "libnet-headers.h"
+#include "libnet-functions.h"
+#include "libnet-macros.h"
+#include "libnet-structures.h"
+#include "libnet-types.h"
+
+#endif
